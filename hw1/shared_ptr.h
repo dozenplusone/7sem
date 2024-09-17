6f84d33c9b2acc4cd653c648e4841e6e856f9d8c
Valery Zabrovsky <valthebrewer@yandex.ru>
2024-09-18 00:21:15 +0300

    shared_ptr: Implement ctors & dtor

--- hw1/shared_ptr.h
+++ hw1/shared_ptr.h
@@ -1,6 +1,8 @@
 #ifndef _HW1_SHARED_PTR_H
 #define _HW1_SHARED_PTR_H
 
+#include <cstddef>
+
 namespace hw1 {
     template<class T>
     class shared_ptr;
@@ -16,6 +18,75 @@ public:
 private:
     element_type *ptr;              // Contained pointer.
     counter_type *refcount;         // Reference counter.
+
+    void acquire(void);
+    void release(void);
+
+public:
+    constexpr shared_ptr(std::nullptr_t = nullptr) noexcept;
+    inline explicit shared_ptr(element_type*);
+    inline shared_ptr(const shared_ptr&) noexcept;
+    inline shared_ptr(shared_ptr&&) noexcept;
+
+    inline ~shared_ptr();
 }; // class hw1::shared_ptr
+
+// Increment reference counter or create one.
+template<class T>
+void hw1::shared_ptr<T>::acquire(void) {
+    if (ptr) {
+        if (!refcount) {
+            refcount = new counter_type();
+        }
+        ++*refcount;
+    }
+}
+
+// Decrement reference counter and, if reaching zero, free data.
+template<class T>
+void hw1::shared_ptr<T>::release(void) {
+    if (!(refcount && --*refcount)) {
+        delete refcount;
+        delete ptr;
+    }
+    ptr = nullptr;
+    refcount = nullptr;
+}
+
+template<class T>
+constexpr hw1::shared_ptr<T>::shared_ptr(std::nullptr_t) noexcept
+    : ptr(nullptr)
+    , refcount(nullptr)
+{}
+
+template<class T>
+hw1::shared_ptr<T>::shared_ptr(element_type *p)
+    : ptr(p)
+    , refcount(nullptr)
+{
+    acquire();
+}
+
+template<class T>
+hw1::shared_ptr<T>::shared_ptr(const shared_ptr &obj) noexcept
+    : ptr(obj.ptr)
+    , refcount(obj.refcount)
+{
+    acquire();
+}
+
+template<class T>
+hw1::shared_ptr<T>::shared_ptr(shared_ptr &&obj) noexcept
+    : ptr(obj.ptr)
+    , refcount(obj.refcount)
+{
+    obj.ptr = nullptr;
+    obj.refcount = nullptr;
+}
+
+template<class T>
+hw1::shared_ptr<T>::~shared_ptr() {
+    release();
+}
 
 #endif // _HW1_SHARED_PTR_H
