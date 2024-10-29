2c6be0223b2f696a6b1d19f88df35b0bfcc16c73
Valery Zabrovsky <valthebrewer@yandex.ru>
2024-10-29 03:32:07 +0300

    Annealing: Implement main algorithm

--- hw2/annealing.h
+++ hw2/annealing.h
@@ -2,8 +2,12 @@
 #define _HW2_ANNEALING_H
 
 #include <cmath>
+#include <ctime>
+#include <random>
 
 namespace hw2 {
+    class Annealing;
+
     class Solution;
     class Mutation;
     class Cooldown;
@@ -15,18 +19,34 @@ namespace hw2 {
     }
 } // namespace hw2
 
+class hw2::Annealing {
+    Solution *best;
+    Mutation *mutation;
+    Cooldown *cooldown;
+
+public:
+    Annealing(Solution *init, Mutation *mut, Cooldown *cd)
+        : best(init)
+        , mutation(mut)
+        , cooldown(cd)
+    {}
+
+    Solution *run(void);
+};
+
 class hw2::Solution {
 public:
     virtual ~Solution() = default;
 
     virtual double criterion(void) const = 0;
+    virtual Solution *copy(void) const = 0;
 };
 
 class hw2::Mutation {
 public:
     virtual ~Mutation() = default;
 
-    virtual void mutate(Solution&) = 0;
+    virtual Solution *mutate(Solution*) = 0;
 };
 
 class hw2::Cooldown {
@@ -70,5 +90,39 @@ public:
         return it ? temp0 * std::log(itp1) / itp1 : temp0;
     }
 };
+
+hw2::Solution *hw2::Annealing::run(void) {
+    std::mt19937 gen(std::time(nullptr));
+    std::uniform_real_distribution<> rand(0., 1.);
+
+    Solution *sol_cur = best->copy();
+    double crit_cur = sol_cur->criterion();
+    unsigned not_improved = 0u;
+
+    for (unsigned it = 0u; not_improved < 100u; ++it) {
+        Solution *sol_new = mutation->mutate(sol_cur);
+        double crit_new = sol_new->criterion();
+
+        double temp = cooldown->get_temp(it);
+        double _diff = crit_cur - crit_new;
+
+        if (_diff >= 0 || rand(gen) < std::exp(_diff / temp)) {
+            std::swap(sol_cur, sol_new);
+            crit_cur = crit_new;
+        }
+
+        delete sol_new;
+
+        if (crit_cur < best->criterion()) {
+            delete best;
+            best = sol_cur->copy();
+        } else {
+            ++not_improved;
+        }
+    }
+
+    delete sol_cur;
+    return best;
+}
 
 #endif // _HW2_ANNEALING_H
